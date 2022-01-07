eacc79b03114eff8523661b9c3c7fad829e75191
余云超 <18113406084@163.com>
2022-01-07 17:17:53 +0800

    feat(add new file): add new file
    add new file

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <MHeader/>
+</template>
+<script>
+import { defineComponent } from 'vue'
+import MHeader from './components/header/header'
+
+export default defineComponent({
+  name: 'App',
+  components: { MHeader }
+})
+</script>
