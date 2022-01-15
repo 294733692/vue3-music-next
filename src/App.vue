df3f6169f0aa82111deb7b9be40580b077b11d43
yuyunchao <294733692@qq.com>
2022-01-15 14:04:14 +0800

    feat(add tab components): add tabs components
    add tabs components in music

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <MHeader/>
+  <Tab />
+  <router-view></router-view>
 </template>
 <script>
 import { defineComponent } from 'vue'
 import MHeader from './components/header/header'
+import Tab from './components/tab/tab'
 
 export default defineComponent({
   name: 'App',
-  components: { MHeader }
+  components: { MHeader, Tab }
 })
 </script>
---
+++ src/views/topList.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>榜单页面</div>
+</template>
+
+<script>
+export default {
+  name: 'topList'
+}
+</script>
+
+<style scoped>
+
+</style>
